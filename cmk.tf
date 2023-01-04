4213bdc31ba8f93d986393827db79a17bd17696a
“Fawaz” <terraform-edu@users.noreply.github.com>
2023-01-05 00:45:37 +0530

    Initial Commit

---
+++ cmk.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+data "aws_caller_identity" "self" {}
+
+data "aws_iam_policy_document" "cmk_admin_policy" {
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["kms:*"]
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_caller_identity.self.arn]
+    }
+  }
+}
+
+resource "aws_kms_key" "cmk" {
+  policy = data.aws_iam_policy_document.cmk_admin_policy.json
+}
+
+resource "aws_kms_alias" "cmk" {
+  name          = "alias/fawaz-kms-cmk-example"
+  target_key_id = aws_kms_key.cmk.id
+}
